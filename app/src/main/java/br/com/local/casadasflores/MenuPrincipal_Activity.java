74e16e954f4d96b97957e70179f43bf270b0e2cb
GUILHERME PEREIRA ANTAO FERNANDES <guilherme.pafernande@senacsp.edu.br>
2022-02-07 11:22:40 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/casadasflores/MenuPrincipal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.casadasflores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_layout);
+    }
+}
